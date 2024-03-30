526b22b14c8ba2792ade1dc7ddd2fef611405686
Azure2212 <Dasanbob22122002@gmail.com>
2024-03-31 03:31:46 +0700

    compeleted file kaggle-github

--- resyslab_utils/clouds/clone_project_by_private_key_ssh.py
+++ resyslab_utils/clouds/clone_project_by_private_key_ssh.py
@@ -1,39 +1,50 @@
 import os
 import subprocess
+from argparse import ArgumentParser
+
+current_path = os.path.abspath(globals().get("__file__","."))
+current_dir = os.path.dirname(current_path)
+
+root_dir = os.path.abspath(f"{current_dir}/../../../")
+
+parser = ArgumentParser()
+parser.add_argument('--giturl',default="",type=str,help="giturl to clone")
+parser.add_argument('--folder',default="",type=str,help="folder clone to")
+parser.add_argument('--branch',default="DEV",type=str,help="branch to clone")
+parser.add_argument('--private-key',default="",type=str,help="private ssh key for authority")
+
+args = parser.parse_args()
+name_key = args.private_key.split("/")[-1]
 
 def init_project(scope=globals(), cfg={}, **kwargs):
    
     import os
     if not os.path.exists('/kaggle/working/resyslab_utils'):
-        print("\033[1;31m---------------you could have to clone resyslab_utils first!---------------\033[0m")
+        print("\033[1;31m---------------you could have cloned resyslab_utils first!---------------\033[0m")
         return
     # init prj_rsa
     rsa_dir = os.path.expanduser('~/.ssh/')
     os.makedirs(rsa_dir, exist_ok=True)
-    subprocess.run(["cp", "/kaggle/input/sgu24-prj-data/data/ssh/sguprj24_rsa", os.path.join(rsa_dir, "sguprj24_rsa")], check=True)
-    subprocess.run(["chmod", "600", os.path.join(rsa_dir, "sguprj24_rsa")], check=True)
+    subprocess.run(["cp", args.private_key, os.path.join(rsa_dir, name_key)], check=True)
+    subprocess.run(["chmod", "600", os.path.join(rsa_dir, name_key)], check=True)
 
     # Configuring SSH for GitHub
-    ssh_config = """
+    ssh_config = f"""
     Host github.com
         HostName ssh.github.com
         User git
         Port 443
         StrictHostKeyChecking no
-        IdentityFile ~/.ssh/sguprj24_rsa
+        IdentityFile ~/.ssh/{name_key}
     """
     with open(os.path.join(rsa_dir, "config"), "w") as f:
         f.write(ssh_config)
 
-    # pull prj repo
-    branch = "dev"
-    folder = "/kaggle/working/sgu24project"
-    giturl = "git@github.com:dntai/sgu24prj.git"
-    if not os.path.exists(folder):
-        subprocess.run(["git", "clone", giturl, folder], check=True)
+    if not os.path.exists(args.folder):
+        subprocess.run(["git", "clone", args.giturl, args.folder], check=True)
     else:
-        subprocess.run(["git", "pull"], cwd=folder, check=True)
-    subprocess.run(["git", "checkout", branch], cwd=folder, check=True)
+        subprocess.run(["git", "pull"], cwd=args.folder, check=True)
+    subprocess.run(["git", "checkout", args.branch], cwd=args.folder, check=True)
 
     if scope is not None:
         scope.update(locals())
